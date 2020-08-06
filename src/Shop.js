4c858102150f7f96461dc29fb224da2cc5bf7b50
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-08-06 23:43:41 +0530

    Added api call and rendered results

--- src/Shop.js
+++ src/Shop.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Shop() {
+    // const app_ID = '391d0a68';
+    // const app_KEY = '16d2e548b6ab2de17518b2eade3837a0';
+
+    useEffect(()=>{
+        fetchItems();
+    },[]);
+
+    const [items, setItems] = useState([]);
+
+const fetchItems = async ()=>{
+    const data = await fetch('https://api.punkapi.com/v2/beers');
+    const items = await data.json();
+    console.log(items);
+    setItems(items);
+}
+
   return (
-    <div className="App">
-        <h1>Shop</h1>
+    <div>
+        {items.map(item=><h3 key={item.id}>{item.name}</h3>)}
     </div>
   );
 }
